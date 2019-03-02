40e6c32a9a3640045b8544a9d36ed7f279befdd3
amir kh <khorsandi@me.com>
2019-03-02 15:04:37 +0100

    fix ipad position for alert view

--- LearnLanguages/Sources/Utilities/UIViewController+AlertController.swift
+++ LearnLanguages/Sources/Utilities/UIViewController+AlertController.swift
@@ -58,7 +58,11 @@ extension UIViewController {
                                           actions: alertActions,
                                           style: .actionSheet,
                                           appendCancel: appendCancel)
-
+        if let popoverController = controller.popoverPresentationController {
+            popoverController.sourceView = self.view //to set the source of your alert
+            popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0) // you can set this as per your requirement.
+            popoverController.permittedArrowDirections = [] //to hide the arrow of any particular direction
+        }
         present(controller, animated: true)
     }
 
