a2b04dda2d0b086e23a5ddce943a961193c98cfe
amir kh <khorsandi@me.com>
2019-03-02 19:16:19 +0100

    Add writing page content

--- LearnLanguages/Sources/Modules/Writing/WritingViewController.swift
+++ LearnLanguages/Sources/Modules/Writing/WritingViewController.swift
@@ -14,135 +14,84 @@ import RxSwift
 import SwiftRichString
 
 class WritingViewController: InputViewController, NibBasedViewController {
-//
-//    // MARK: Constants
-//
-//    private struct Constants {
-//
-//        static let speakingHint = "[Start typing ...]"
-//        static let autoGoToTheNextWithPercentage = 90
-//        static let autoGoToTheNextDelay: Double = 2
-//    }
-//
-//
-//    // MARK: Properties
-//
-//    override var preferredStatusBarStyle: UIStatusBarStyle {
-//        return .lightContent
-//    }
-//
-//    private var playerController: PlayerViewController!
-//    private var subtitleRepository: SubtitleRepository!
-//    private var fileRepository: FileRepository!
-//    private var disposeBag = DisposeBag()
-//    private var currentSubtitle: String?
-//    private var typingIsAllowed: Bool = false
-//
-//
-//    // MARK: Outlets
-//
-//    @IBOutlet private weak var subtitleLabelView: UILabel!
-//    @IBOutlet private weak var inputTextView: UITextView!
-//    @IBOutlet private weak var playerContainerView: UIView!
-//    @IBOutlet private weak var playPauseButton: UIButton!
-//    @IBOutlet private weak var correctPercentageLabel: UILabel!
-//    @IBOutlet private weak var doneButton: UIButton!
-//    @IBOutlet private weak var hintButton: UIButton!
-//    @IBOutlet private weak var replayButton: UIButton!
-//
-//
-//    // MARK: Lifecyle
-//
-//    public override func viewDidLoad() {
-//        super.viewDidLoad()
-//        fileRepository = FileRepository()
-//
-//        configureSpeechRecognizerService()
-//        configureRecordButton()
-//        addPlayerViewController()
-//        subscribeToPlayerTime()
-//        adjustResultViewIfNeeded()
-//        configureSubtitleRepositoryAndThenPlay()
-//    }
-//
-//    override public func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//
-//    }
-//
-//    override func viewWillDisappear(_ animated: Bool) {
-//        super.viewWillDisappear(animated)
-//        playerController.pause()
-//    }
-//
-//
-//    // MARK: - Event handlers
-//
-//    @IBAction private func playPauseButtonTouched() {
-//        playerController.togglePlaying()
-//    }
-//
-//    @IBAction private func skipNextButtonTouched() {
-//        let time = subtitleRepository.getStartOfNextSubtitle(currentTime: playerController.playerTime)
-//        seek(to: time)
-//    }
-//
-//    @IBAction private func skipPrevButtonTouched() {
-//        let time = subtitleRepository.getStartOfPrevSubtitle(currentTime: playerController.playerTime)
-//        seek(to: time)
-//    }
-//
-//    @IBAction private func doneButtonTouched() {
-//
-//    }
-//
-//    @IBAction private func hintButtonTouchedDown() {
-//        showHint()
-//    }
-//
-//    @IBAction private func hintButtonTouchedUpOutside() {
-//        hideHint()
-//    }
-//
-//    @IBAction private func hintButtonTouchedUpInside() {
-//        hideHint()
-//    }
-//
-//    @IBAction private func onReplayButtonTouched() {
-//        replay()
-//    }
-//
-//
-//    // MARK: Private functions
-//
-//    private func seek(to time: Double) {
-//        playerController.seek(to: time)
-//        playerController.play()
-//    }
-//
-//    private func showHint() {
-//        playerController.pause()
-//        stopRecordingIfNeeded()
-//        textLabelTempValue = textLabelView.text
-//        updateTextLabelView(currentSubtitle)
-//    }
-//
-//    private func hideHint() {
-//        if let tempValue = textLabelTempValue {
-//            updateTextLabelView(tempValue)
-//            textLabelTempValue = nil
+
+    // MARK: Properties
+
+    override var isInputBusy: Bool {
+        return false// TODO: set language
+    }
+
+
+    // MARK: Outlets
+
+    @IBOutlet private weak var inputTextView: UITextView!
+    @IBOutlet private weak var doneButton: UIButton!
+
+
+    // MARK: Lifecyle
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        adjustResultViewIfNeeded()
+    }
+
+    override public func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        inputTextView.becomeFirstResponder()
+    }
+
+
+    // MARK: - Event handlers
+
+    @IBAction private func doneButtonTouched() {
+       adjustResultViewIfNeeded(input: inputTextView.text)
+    }
+
+
+    // MARK: Internal functions
+
+    internal override func togglePlayerPlaying() {
+        super.togglePlayerPlaying()
+        inputTextView.text = ""
+    }
+
+    internal override func showHint() {
+        super.showHint()
+        inputTextView.isHidden = true
+    }
+
+    internal override func hideHint() {
+        super.hideHint()
+        inputTextView.isHidden = false
+    }
+
+    internal override func replay() {
+        super.replay()
+        inputTextView.text = ""
+    }
+
+    internal override func seek(to time: Double) {
+        super.seek(to: time)
+        inputTextView.text = ""
+    }
+
+    internal override func onReadyToGetNewInput() {
+        inputTextView.text = ""
+        inputTextView.becomeFirstResponder()
+    }
+
+    internal override func onInputAllowedChanged(isAllowed: Bool) {
+//        recordButton.isEnabled = isAllowed
+//        if !isAllowed {
+//            textLabelView.text = ""
 //        }
-//    }
-//
-//    private func replay() {
-//        guard !playerController.isPlaying,
-//            let time = subtitleRepository.getStartOfCurrentSubtitle() else {
-//                return
-//        }
-//        autoStartRecordingForNext = false
-//        stopRecordingIfNeeded(shouldKeepResult: false)
-//        subtitleRepository.cleanLastStop()
-//        playerController.seek(to: time)
-//        playerController.play()
-//    }
+    }
+
+}
+
+
+extension WritingViewController: UITextFieldDelegate {
+
+
 }
